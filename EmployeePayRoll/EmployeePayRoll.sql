47486df216b2fc56d43d1d4525281f4beec730be
chaitanya4294 <chaitanya.4294@gmail.com>
2021-10-22 23:44:58 +0530

    [Chaitanya] Add : Added UC3 to add data into employee_payroll table

--- EmployeePayRoll/EmployeePayRoll.sql
+++ EmployeePayRoll/EmployeePayRoll.sql
@@ -16,6 +16,12 @@ Salary INT,
 StartDate DATE)
  
 -- UC3 => Create employee_payroll data
+INSERT INTO employee_payroll VALUES ('Chaitanya', 10000, '2021-09-09')
+INSERT INTO employee_payroll VALUES ('Mabel', 20000, '2021-10-09')
+INSERT INTO employee_payroll VALUES ('Mounika', 30000, '2020-11-09')
+INSERT INTO employee_payroll VALUES ('Nibedita', 40000, '2019-08-08')
+INSERT INTO employee_payroll VALUES ('Manish', 50000, '2018-07-07')
+INSERT INTO employee_payroll VALUES ('Kumar', 60000, '2019-06-06')
 
 -- UC4 => Select and print all data in employee_payroll table
 
