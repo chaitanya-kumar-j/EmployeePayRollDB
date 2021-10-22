dde8d424d4b65c973c039c9dea634bc3bd174a59
chaitanya4294 <chaitanya.4294@gmail.com>
2021-10-22 22:41:45 +0530

    [Chaitanya] Add : Added UC1 to create database, to show databases, to select database

---
+++ EmployeePayRoll/EmployeePayRoll.sql
@@ -0,0 +1,9 @@
+-- UC1- Create database and show database
+-- Create the database
+CREATE DATABASE payroll_service
+
+-- view database
+SELECT name FROM master.sys.databases
+
+-- selecting database
+USE payroll_service
